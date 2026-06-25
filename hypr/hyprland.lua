f3d3bed2492f4928aec5d4676932d20b65fb1bde
autuxmn-leaf <autuxmn.leaf@proton.me>
2026-06-24 21:59:22 -0400

    Add files via upload

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -44,7 +44,29 @@ hl.bind("SUPER+Q", hl.dsp.window.close())
 hl.bind("SUPER+SHIFT+Q", hl.dsp.window.kill())
 hl.bind("SUPER+W", hl.dsp.exec_cmd("firefox"))
 hl.bind("SUPER+E", hl.dsp.exec_cmd("nautilus"))
+hl.bind("SUPER+1", hl.dsp.focus({workspace=1}))
+hl.bind("SUPER+2", hl.dsp.focus({workspace=2}))
+hl.bind("SUPER+3", hl.dsp.focus({workspace=3}))
+hl.bind("SUPER+4", hl.dsp.focus({workspace=4}))
+hl.bind("SUPER+5", hl.dsp.focus({workspace=5}))
+hl.bind("SUPER+6", hl.dsp.focus({workspace=6}))
+hl.bind("SUPER+7", hl.dsp.focus({workspace=7}))
+hl.bind("SUPER+8", hl.dsp.focus({workspace=8}))
+hl.bind("SUPER+9", hl.dsp.focus({workspace=9}))
+hl.bind("SUPER+0", hl.dsp.focus({workspace=10}))
+hl.bind("SUPER+ALT+1", hl.dsp.window.move({workspace=1,follow=false}))
+hl.bind("SUPER+ALT+2", hl.dsp.window.move({workspace=2,follow=false}))
+hl.bind("SUPER+ALT+3", hl.dsp.window.move({workspace=3,follow=false}))
+hl.bind("SUPER+ALT+4", hl.dsp.window.move({workspace=4,follow=false}))
+hl.bind("SUPER+ALT+5", hl.dsp.window.move({workspace=5,follow=false}))
+hl.bind("SUPER+ALT+6", hl.dsp.window.move({workspace=6,follow=false}))
+hl.bind("SUPER+ALT+7", hl.dsp.window.move({workspace=7,follow=false}))
+hl.bind("SUPER+ALT+8", hl.dsp.window.move({workspace=8,follow=false}))
+hl.bind("SUPER+ALT+9", hl.dsp.window.move({workspace=9,follow=false}))
+hl.bind("SUPER+ALT+0", hl.dsp.window.move({workspace=10,follow=false}))
 hl.bind("SUPER+D",hl.dsp.window.fullscreen({mode = "maximized"}))
+hl.bind("SUPER+F",hl.dsp.window.fullscreen())
+hl.bind("SUPER+mouse:273", hl.dsp.window.resize())
 
 ------------
 -- CONFIG --
@@ -77,6 +99,7 @@ hl.config({
 	
 	input = {
 		touchpad = {
+			clickfinger_behavior = true,
 			natural_scroll = true
 		},
 		sensitivity = -0.2,
@@ -96,5 +119,19 @@ hl.layer_rule({
   blur = true,
   blur_popups = true,
 })
-
-
+---------------
+-- ANIMATION -- 
+---------------
+hl.curve(
+	"penis",
+	{
+		type = "bezier",
+		points = {{0.83, 0}, { 0.17, 1}}
+	}
+)
+hl.animation({
+	leaf = "windows",
+	enabled = true,
+	speed = 1,
+	bezier = "penis"
+})
