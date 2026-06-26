408d856bb9e2fc683e4b3beca8b8dcdc7199f68e
autuxmn-leaf <autuxmn.leaf@proton.me>
2026-06-25 22:09:23 -0400

    Add files via upload

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -16,6 +16,7 @@ local ipc = "noctalia msg"
 hl.on("hyprland.start", function ()
 	hl.exec_cmd("noctalia")
 	hl.exec_cmd("hypridle")
+	hl.exec_cmd("systemctl --user start hyprpolkitagent")
 end)
 --------------
 -- MONITORS --
